e4dc68bb31d30485d6b6c2df7915a6e6a734e6f9
bobgodwinx <bobgodwinx@gmail.com>
2019-06-05 16:00:16 +0200

    Initial introduction of RootView

---
+++ TechExecutives/Views/RootView.swift
@@ -0,0 +1,23 @@
+//
+//  RootView.swift
+//  TechExecutives
+//
+//  Created by Bob Godwin Obi on 05.06.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct RootView_Previews : PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
+#endif
