5d08f3d157c163396ca1a5d58b302aec8c3f8203
bobgodwinx <bobgodwinx@gmail.com>
2019-06-05 17:00:29 +0200

    Initial introduction of DetailView

---
+++ TechExecutives/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  TechExecutives
+//
+//  Created by Bob Godwin Obi on 05.06.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct DetailView_Previews : PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
+#endif
