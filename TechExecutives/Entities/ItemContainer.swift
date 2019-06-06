5baad312e7328027a5cebf3a8c57385c2ea906de
bobgodwinx <bobgodwinx@gmail.com>
2019-06-06 08:14:29 +0200

    Initial introduction of ItemContainer

---
+++ TechExecutives/Entities/ItemContainer.swift
@@ -0,0 +1,13 @@
+//
+//  ItemContainer.swift
+//  TechExecutives
+//
+//  Created by Bob Godwin Obi on 06.06.19.
+//  Copyright © 2019 Bob Godwin Obi. All rights reserved.
+//
+
+import Foundation
+
+struct ItemContainer: Decodable {
+    let items: [Executive]
+}
